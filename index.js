c5aefcbb2c77b404c7dc47666f155b73097ad785
Prathmesh6968 <prathmeshcoder69@gmail.com>
2026-02-09 19:03:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post("/ask", async (req, res) => {
     }
 
     const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=AIzaSyBWbmYbYgfRt4303jaRyVe8cG7bj28_cdM`,
       {
         contents: [
           {
